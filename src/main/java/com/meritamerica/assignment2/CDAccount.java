d1ac5b2e26c2e406f2cf67dd3601d2e2dd37b8b2
wesley <30338320+wasman100@users.noreply.github.com>
2020-10-10 17:41:41 -0400

    Update CDAccount.java

--- src/main/java/com/meritamerica/assignment2/CDAccount.java
+++ src/main/java/com/meritamerica/assignment2/CDAccount.java
@@ -3,32 +3,40 @@ package com.meritamerica.assignment2;
 import java.util.Date;
 
 public class CDAccount {
-	public double balance =0;
-	public double intrestRate =0;
-	public int term =0;
-	public Date StartDate =null;
-	public long accountNumber;
-
-	public CDAccount(CDOffering offering, double balance) {
+	CDOffering offering;
+	Date date;
+	private double balance;
+	private long accountNumber;
 		
+	public CDAccount(CDOffering offering, double openBalance){
+		this.date = new Date();
+		this.offering = offering;
+		this.balance = openBalance;
+		this.accountNumber = MeritBank.getNextAccountNumber();
 	}
+	
 	public double getBalance() {
 		return balance;
 	}
+	
 	public double getInterestRate() {
-		return intrestRate;
+		return offering.getInterestRate();
 	}
+	
 	public int getTerm() {
-		return term;
+		return offering.getTerm();
 	}
-	public Date getStartDate() {
-		return StartDate;
+	
+	public java.util.Date getStartDate(){
+		return date;
 	}
+	
 	public long getAccountNumber() {
-		return accountNumber;
+		return accountNumber;	
 	}
+	
 	public double futureValue() {
-		
+		return balance*Math.pow(1 + getInterestRate(), getTerm());
 	}
 
 }
