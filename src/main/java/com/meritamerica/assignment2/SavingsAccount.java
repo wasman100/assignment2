d8749b4d74b9b0b0fce8af79abd020e9258e1f0f
wesley <30338320+wasman100@users.noreply.github.com>
2020-10-10 15:09:41 -0400

    Update SavingsAccount.java

--- src/main/java/com/meritamerica/assignment2/SavingsAccount.java
+++ src/main/java/com/meritamerica/assignment2/SavingsAccount.java
@@ -2,15 +2,9 @@ package com.meritamerica.assignment2;
 
 public class SavingsAccount extends BankAccount {
 
-	public SavingsAccount(long accountNumber, double balance, double interestRate) {
-		super(accountNumber, balance, interestRate);
-		
+	public SavingsAccount(double balance) {
+		super(balance);
 	}
-	public SavingsAccount(double balance, double interestRate) {
-		super(balance, interestRate);
-
-	}
-
 
 
 }
